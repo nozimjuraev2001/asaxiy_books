faddc54555d9d11ceb6b01d5c9b65ba01a4f98ee
NozimbekJuraev <nozimjuraev1699@gmail.com>
2022-10-28 16:54:40 +0500

    added swagger

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
     'rest_framework',
     'django_password_validators',
     'django_password_validators.password_history',
